e2ebc501b3594193049b068dea6a538cbfe59c9a
Arik <arikshalito@gmail.com>
2025-01-30 19:35:22 +0200

    SetJobFail

--- JobExecutor/Program.cs
+++ JobExecutor/Program.cs
@@ -7,13 +7,7 @@ namespace JobExecutor
         static void Main(string[] args)
         {
             JobStateManager jobStateManager = new JobStateManager();
-
-            //jobStateManager.GetNextAvailableJobs();
-            //jobStateManager.SetJobFail(1);
-            //jobStateManager.SetJobSuccessful(1);
-
-            // Example #1
-            jobStateManager.Init(new List<JobInput>
+            List<JobInput> jobInputs = new List<JobInput>
             {
                     new JobInput { Job = 2, DependsOn = 1 },
                     new JobInput { Job = 3, DependsOn = 1 },
@@ -24,27 +18,25 @@ namespace JobExecutor
                     new JobInput { Job = 10, DependsOn = 8 },
                     new JobInput { Job = 7, DependsOn = 5 },
                     new JobInput { Job = 7, DependsOn = 6 }
-            });
+            };
+
+          
+
+            // Example #1
+            jobStateManager.Init(jobInputs);
             Console.WriteLine(string.Join(",", jobStateManager.GetNextAvailableJobs()));
 
             // Example #2
-            jobStateManager.Init(new List<JobInput>
-            {
-                    new JobInput { Job = 2, DependsOn = 1 },
-                    new JobInput { Job = 3, DependsOn = 1 },
-                    new JobInput { Job = 4, DependsOn = 2 },
-                    new JobInput { Job = 4, DependsOn = 3 },
-                    new JobInput { Job = 8, DependsOn = 4 },
-                    new JobInput { Job = 9, DependsOn = 8 },
-                    new JobInput { Job = 10, DependsOn = 8 },
-                    new JobInput { Job = 7, DependsOn = 5 },
-                    new JobInput { Job = 7, DependsOn = 6 }
-            });
+            jobStateManager.Init(jobInputs);
             jobStateManager.SetJobSuccessful(1);
             Console.WriteLine(string.Join(",", jobStateManager.GetNextAvailableJobs()));
 
-            //jobStateManager.SetJobFail(1);
-            //jobStateManager.GetNextAvailableJobs();
+            // Example #3
+            jobStateManager.Init(jobInputs);
+            jobStateManager.SetJobFail(1);
+            Console.WriteLine(string.Join(",", jobStateManager.GetNextAvailableJobs()));
+
+          
 
         }
     }
